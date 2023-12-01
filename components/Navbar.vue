5b0bc6db45aa22a9c364f9539a54da742c4d995e
MDafaAzharya <125567760+MDafaAzharya@users.noreply.github.com>
2023-12-01 13:24:27 +0700

    first notes-web commit

---
+++ components/Navbar.vue
@@ -0,0 +1,29 @@
+<script>
+import "../assets/css/index.css";
+</script>
+<template>
+    <!-- Navbar -->
+    <nav
+      class="top-0 left-0 w-full z-10 bg-sky-400 md:flex-row md:flex-nowrap md:justify-startitems-center p-4 shadow-lg hidden md:block"
+    >
+      <div
+        class="w-full mx-autp items-center flex justify-between md:flex-nowrap flex-wrap md:px-10 px-4"
+      >
+        <!-- Brand -->
+        <p
+          class="title text-white text-lg  hidden lg:inline-block font-bold"
+        >
+          Welcome !!!
+        </p>
+        <!-- Form -->
+        <div>
+            <NuxtLink to="/dashboard/profile"><img src="../assets/img/profile.png" alt="" width="40"></NuxtLink>
+        </div>
+        <!-- User -->
+      </div>
+    </nav>
+    <!-- End Navbar -->
+  </template>
+  
+ 
+  
---
+++ layouts/dashboard.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+      <Sidebar />
+      <div class="relative md:ml-64 bg-blueGray-100">
+        <Navbar />
+          <slot />
+          <End/>
+        </div>
+    </div>
+  </template>
